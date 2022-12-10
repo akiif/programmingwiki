3840d9ec261388719dfc1c570b677ddd9d1009a8
Akif Mohammed <akiif.dev@gmail.com>
2022-12-10 14:31:56 +0530

    Upated the api url and replaced node-fetch with axios

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
-import fetch from "node-fetch";
+import axios from 'axios';
 
-const url = "https://programmingwiki.herokuapp.com/";
+const url = "https://programmingwiki.up.railway.app/";
 
 async function all() {
-    return await fetch(url + "articles").then((response) => response.json());
+  return await
+  axios.get(url + "articles")
+    .then((response) => response.data)
+    .catch((error) => ({
+      "error": "Unable to fetch the query"
+    }));
 }
+
 async function random() {
-    return await fetch(url + "random").then((response) => response.json());
+  return await
+  axios.get(url + "random")
+    .then((response) => response.data)
+    .catch((error) => ({
+      "error": "Unable to fetch the query"
+    }));
 }
 
 async function searchByTitle(title) {
-    return await fetch(
-        url +
-            "articles?" +
-            new URLSearchParams({
-                q: title,
-            })
-    ).then((response) => response.json());
+  return await
+  axios.get(
+      url +
+      "articles?" +
+      new URLSearchParams({
+        q: title,
+      })
+    )
+    .then((response) => response.data)
+    .catch((error) => ({
+      "error": "Unable to fetch the query"
+    }));
 }
 
 async function searchByTags(tags) {
-    let search = new URLSearchParams(tags.map((s) => ["tags", s]));
-    let searchString = search.toString();
-    return await fetch(url + "articles?" + searchString).then((response) =>
-        response.json()
-    );
+  let search = new URLSearchParams(tags.map((s) => ["tags", s]));
+  let searchString = search.toString();
+  return await
+  axios.get(url + "articles?" + searchString)
+    .then((response) => response.data)
+    .catch((error) => ({
+      "error": "Unable to fetch the query"
+    }));
 }
 
 export default all;
-export { all, random, searchByTitle, searchByTags };
+export {
+  all,
+  random,
+  searchByTitle,
+  searchByTags
+};
